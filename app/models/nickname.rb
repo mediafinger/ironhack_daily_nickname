fba9dce6694ec70513be5b431cfe0aea76de13cb
Andreas Finger <andreas.finger@xing.com>
2014-08-02 19:44:52 +0200

    Make sure nickname is unique per day

--- app/models/nickname.rb
+++ app/models/nickname.rb
@@ -1,11 +1,15 @@
 class Nickname < ActiveRecord::Base
   belongs_to :user
 
-  validates :name, presence: true, uniqueness: {
-    case_sensitive: false,
-    message: "The nickname must be unique."
-  }
-
   scope :current, -> { where(updated_at: Date.today.beginning_of_day..Date.today.end_of_day) }
+
+  validates :name, presence: true
+  validate  :name_has_to_be_unique_per_day
+
+  def name_has_to_be_unique_per_day
+    if Nickname.current.pluck("name").include? name
+      errors.add(:name, "The nickname must be unique per day.")
+    end
+  end
 
 end
