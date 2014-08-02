3692e5d36098d908a96faadcfb1f3d33ffeb2183
Andreas Finger <andreas.finger@xing.com>
2014-08-02 21:25:52 +0200

    Hints on how to fix a bug or find another solution

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,14 @@ class User < ActiveRecord::Base
   validates :name, presence: true
 
   def nickname
+    # this will break when nicknames.current returns nil
     nicknames.current.first.name
+
+    # so either call:
+    #   nicknames.current.first.try(:name)
+
+    # or create a name if none is present
+    #   nick = nicknames.current.first
+    #   nick.present? ? nick.name : nicknames.create!(name: NameGeneratorService.new.generate).name
   end
 end
