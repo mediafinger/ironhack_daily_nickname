eeb86145b23dfb7bd1f355a4b48bb95e4660ea6b
Andreas Finger <andreas.finger@xing.com>
2014-08-02 20:26:59 +0200

    Make sure comparison is case insensitive and performs well

--- app/models/nickname.rb
+++ app/models/nickname.rb
@@ -7,7 +7,14 @@ class Nickname < ActiveRecord::Base
   validate  :name_has_to_be_unique_per_day
 
   def name_has_to_be_unique_per_day
-    if Nickname.current.pluck("name").include? name
+    # case insensitive comparison
+    # downcasing a whole array comes with poor performance if it has many elements
+    #   Nickname.current.pluck("name").map(&:downcase).include? name.downcase
+    # might be ok, if run as nightly cron-job
+    # this alternative might perform faster under specific cirumstances:
+    #   Nickname.current.pluck("name").any?{ |nick| nick.casecmp(name) == 0 }
+    # faster should be a direct SQL query
+    if Nickname.current.where("LOWER(name) LIKE ?", name.downcase).first.present?
       errors.add(:name, "The nickname must be unique per day.")
     end
   end
